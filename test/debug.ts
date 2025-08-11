84f2467bf498955ba37a8db6d3f539c1d0930bac
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-08-11 15:26:42 +0200

    chore: cleanup

--- test/debug.ts
+++ test/debug.ts
@@ -27,50 +27,6 @@ const argv = yargs(hideBin(process.argv))
 
 const filenameStr = String(argv.filename);
 
-// const rs = createReadStream(filenameStr, {
-// 	encoding: "utf8",
-// });
-// const source: UnderlyingSource<string> = {
-// 	start(controller) {
-// 		rs.on("data", (chunk: any) => {
-// 			controller.enqueue(chunk);
-// 		});
-// 		rs.on("end", () => {
-// 			controller.close();
-// 		});
-// 		rs.on("error", (err) => {
-// 			controller.error(err);
-// 		});
-// 	},
-// 	cancel() {
-// 		rs.removeAllListeners();
-// 		rs.destroy();
-// 	},
-// };
-// const readable = new ReadableStream<string>(source);
-
-// const parser = new CompleteLogEntries();
-// const unfmt = new UnformatLogEntry();
-// const filter = new FilterLogEntries();
-// const parseNested = new ParseNestedStructures();
-// const classify = new ClassifyLogEntry();
-
-// const iter = readable
-// 	.pipeThrough(parser)
-// 	.pipeThrough(unfmt)
-// 	.pipeThrough(parseNested)
-// 	.pipeThrough(filter)
-// 	.pipeThrough(classify)
-// 	// TODO: Group related entries somehow
-// 	.values();
-
-// for await (const chunk of iter) {
-// 	if (argv.pretty) {
-// 		console.dir(chunk, { depth: Infinity });
-// 	} else {
-// 		console.log(JSON.stringify(chunk));
-// 	}
-// }
 
 const log = await fs.readFile(filenameStr, "utf8");
 const pipeline = new LogTransformPipeline();
